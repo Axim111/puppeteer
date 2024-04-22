4fcd29497676ea56addf6ac9db881ad38aeef8e8
Axim <makseropolov@gmail.com>
2024-04-22 09:53:39 +0500

    getAttribute

--- index.js
+++ index.js
@@ -1,19 +1,63 @@
 import puppeteer from 'puppeteer';
 
+
+let scrollDown = (async (page) => {
+  let prevHeight = -1;
+  let maxScrolls = 100;
+  let scrollCount = 0;
+
+  while (scrollCount < maxScrolls) {
+    // Scroll to the bottom of the page
+    await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+    // Wait for page load
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    // Calculate new scroll height and compare
+    let newHeight = await page.evaluate('document.body.scrollHeight');
+    if (newHeight == prevHeight) {
+      break;
+    }
+    prevHeight = newHeight;
+    scrollCount += 1;
+  }
+
+})
+
+
+
 let main = (async () => {
+  // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
     headless: false,
     ignoreHTTPSErrors: true,
     defaultViewport: false,
   });
+
+
   const page = await browser.newPage();
   await page.goto("https://kwork.ru/projects")
-  const jobs = await page.$$(".project-list")
-  for (let job of jobs) {
-    console.log(await job.evaluate(node =>
-      node.querySelector(".mb15 > .d-flex > .wants-card__left")
-        .innerText))
-  }
+  // scrollDown(page)
+  const jobs = await page.$$(".card")
+
+
+  // for (let job of jobs) {
+  //   // let job = await page.evaluate(el => el.innerText)
+  //   console.log(await job.evaluate(node =>
+  //     node.querySelector(".card__content")
+  //       .innerText))
+
+  // }
+
+  let tem = await (await page.$(".paging")).evaluate(node =>
+    node.querySelector(".next > a").getAttribute("href"))
+
+
+
+
+
+
+
+
+
 
 });
 main();
